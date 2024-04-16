d4f8e966c80c23b5f6820e3a9a6cd763b5428d5a
steve-h <stevelist@hume.ca>
2024-04-16 19:10:24 -0400

    Move the trailing <script> chunks to the end of <body> from end of <html>

--- Sources/Ignite/Elements/Body.swift
+++ Sources/Ignite/Elements/Body.swift
@@ -25,13 +25,27 @@ public struct Body: PageElement, HTMLRootElement {
     /// - Parameter context: The current publishing context.
     /// - Returns: The HTML for this element.
     public func render(context: PublishingContext) -> String {
-        let output = Group {
+        var output = Group {
             for item in items {
                 item
             }
         }
         .class("col-sm-\(context.site.pageWidth)", "mx-auto")
         .render(context: context)
+
+        output += Script(file: "/js/bootstrap.bundle.min.js").render(context: context)
+
+        if context.site.syntaxHighlighters.isEmpty == false {
+            output += Script(file: "/js/syntax-highlighting.js").render(context: context)
+        }
+
+        // Activate tooltips if there are any.
+        if output.contains(#"data-bs-toggle="tooltip""#) {
+            output += Script(code: """
+            const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+            const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+            """).render(context: context)
+        }
 
         return "<body\(attributes.description)>\(output)</body>"
     }
