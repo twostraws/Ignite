54f0e7f4469999a7cfd9aaf354853d91f68d1c7b
Piotr Jeremicz <piotr.jeremicz@nestbank.pl>
2024-04-18 15:44:49 +0200

    Created `EnvironmentValues` as main value store

---
+++ Sources/Ignite/Publishing/Environment/EnvironmentValues.swift
@@ -0,0 +1,24 @@
+//
+// EnvironmentValues.swift
+// Ignite
+// https://www.github.com/piotrekjeremicz/Ignite
+// See LICENSE for license information.
+//
+
+import Foundation
+
+public struct EnvironmentValues {
+    private var everyValue: [ObjectIdentifier: Any] = [:]
+
+    public init() {}
+
+    public subscript<K>(key: K.Type) -> K.Value where K : EnvironmentKey {
+        get {
+            guard let value = everyValue[ObjectIdentifier(key)] as? K.Value else { return key.defaultValue }
+            return value
+        }
+        set { 
+            everyValue[ObjectIdentifier(key)] = newValue
+        }
+    }
+}
