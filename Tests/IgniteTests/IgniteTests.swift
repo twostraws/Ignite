f24617ea4daca66f6f6fbad7eb6ad3e71a22ae44
Gavin Eadie <gavin@umich.edu>
2024-05-01 16:17:17 -0400

    add unit tests for impacted subsite elements ..

--- Tests/IgniteTests/IgniteTests.swift
+++ Tests/IgniteTests/IgniteTests.swift
@@ -4,6 +4,10 @@ import XCTest
 
 /// A base class that sets up an example publishing context for testing purposes.
 class ElementTest: XCTestCase {
-    /// A publishing context with sample values.
-    let publishingContext = try! PublishingContext(for: TestSite(), rootURL: URL.documentsDirectory)
+    /// A publishing context with sample values for root site tests.
+    let publishingContext = try! PublishingContext(for: TestSite(), 
+                                                   rootURL: URL.documentsDirectory)
+    /// A publishing context with sample values for subsite tests.
+    let publishingSubsiteContext = try! PublishingContext(for: TestSubsite(),
+                                                          rootURL: URL.documentsDirectory)
 }
