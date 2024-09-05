a9c5855973a9649a74cb2dd59fd5612369f251fb
Dalton Alexandre <166029845+dl-alexandre@users.noreply.github.com>
2024-09-05 16:46:12 -0700

    Fix AudioTest Failure

--- Tests/IgniteTests/Elements/Audio.swift
+++ Tests/IgniteTests/Elements/Audio.swift
@@ -18,19 +18,16 @@ final class AudioTests: ElementTest {
 
         XCTAssertEqual(output,
         """
-        <audio controls>
-        <source src="/audio/example.mp3" type="audio/mp3">
-        Your browser does not support the audio element.
-        </audio>
+        <audio controls><source src="/audio/example.mp3" type="audio/mp3">Your browser does not support the audio element.</audio>
         """)
     }
     func test_multiFileAudio() {
-        let element = Audio("/audio/example1.mp3", "/audio/example2.m4a")
+        let element = Audio("/audio/example1.mp3", "/audio/example2.wav")
         let output = element.render(context: publishingContext)
 
         XCTAssertEqual(output,
         """
-        <audio controls><source src="/audio/example1.mp3" type="audio/mp3"><source src="/audio/example2.m4a" type="audio/m4a">Your browser does not support the audio element.</audio>
+        <audio controls><source src="/audio/example1.mp3" type="audio/mp3"><source src="/audio/example2.wav" type="audio/wav">Your browser does not support the audio element.</audio>
         """)
     }
 }
