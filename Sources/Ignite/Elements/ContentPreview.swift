e452f1c156ab8148fbbb88d6c61062ae517c5083
sookim-1 <scstnghks@gmail.com>
2024-09-27 10:54:48 +0900

    convert closure to style protocol

--- Sources/Ignite/Elements/ContentPreview.swift
+++ Sources/Ignite/Elements/ContentPreview.swift
@@ -7,6 +7,11 @@
 
 import Foundation
 
+/// A protocol for customizing the layout of ContentPreview.
+public protocol ContentPreviewStyle {
+    func body(content: Content, context: PublishingContext) -> BlockElement
+}
+
 /// A wrapper around Card, specifically aimed at presenting details about
 /// some content on your site. This automatically links to your content page
 /// and adds in tags.
@@ -19,25 +24,29 @@ public struct ContentPreview: BlockElement {
     /// How many columns this should occupy when placed in a section.
     public var columnWidth = ColumnWidth.automatic
 
-    /// The layout block element for rendering the preview. Defaults to a card if not provided.
-    public var customLayout: ((Content) -> BlockElement)?
+    /// Custom style for the content preview.
+    private var style: ContentPreviewStyle?
 
-    /// Initializes the content preview with an optional custom layout.
+    /// Initializes the content preview
     /// - Parameters:
     ///   - content: The content to display.
-    ///   - customLayout: An optional closure for custom layout.
-    public init(for content: Content, customLayout: ((Content) -> BlockElement)? = nil) {
+    public init(for content: Content) {
         self.content = content
-        self.customLayout = customLayout
+    }
+
+    public func contentPreviewStyle<S: ContentPreviewStyle>(_ style: S) -> ContentPreview {
+        var copy = self
+        copy.style = style
+        return copy
     }
 
     /// Renders the content preview with either a custom layout or the default card.
     /// - Parameter context: The publishing context for rendering.
     /// - Returns: A rendered string of HTML.
     public func render(context: PublishingContext) -> String {
-        // Rendering using either custom layout or default card layout.
-        if let customLayout {
-            return customLayout(content)
+        // If custom style is provided, use it; otherwise, fallback to default layout.
+        if let style = style {
+            return style.body(content: content, context: context)
                 .attributes(attributes)
                 .render(context: context)
         } else {
