7989d3e55e209bbe2fb186aab02a671825b6c695
Peter van den Hamer <13396568+vdhamer@users.noreply.github.com>
2024-10-12 18:38:38 +0200

    Prevent compiler error about async/await requiring iOS 13

--- Sources/Ignite/Framework/Theme.swift
+++ Sources/Ignite/Framework/Theme.swift
@@ -10,5 +10,6 @@ import Foundation
 /// Themes allow you to have complete control over the HTML used to generate
 /// your pages.
 public protocol Theme {
+    @available(iOS 13.0.0, *)
     func render(page: Page, context: PublishingContext) async -> HTML
 }
