f15282ed891b82705c31ede506e996fd75182f6e
Peter van den Hamer <13396568+vdhamer@users.noreply.github.com>
2024-10-13 00:01:48 +0200

    Cleanup of URL-SelectDirectories.swif file. No functional changes. Added more documentation.

--- Sources/Ignite/Extensions/URL-SelectDirectories.swift
+++ Sources/Ignite/Extensions/URL-SelectDirectories.swift
@@ -9,37 +9,43 @@ import Foundation
 
 extension URL {
     /// Returns URL where to find Assets/Content/Includes and URL where to generate the static web site.
-    /// When building a package, the website is built at the source URL (and both URLs are equal).
-    /// When building a MacOS app, the website is built in a subdirectory of the app's sandbox.
+    /// For now both URLs always equal:
+    ///     * A standalone Ignite package doesn't generate a container/sandbox to hold the app's output website.
+    ///     * Ignite as a package in a regular app cannot read or write to the source location (sandbox/security).
+    /// In both cases, ensure that Assets/Content/Include data can be found at buildDirectoryURL.
     /// - Parameter file: path of a Swift source file to find source root directory by scanning path upwards.
-    /// - Returns tupple containing source URL and URL where output is built.
-    public static func selectDirectories(from file: StaticString) throws -> SourceBuildDirectories {
+    /// - Returns struct containing source URL and URL where output is built.
+    public static func selectDirectories(from file: StaticString) throws -> SourceAndBuildDirectories {
         var currentURL = URL(filePath: file.description)
 
+        // try to see if we are building a standalone package (and can thus find Package.swift somewhere in path))
         repeat {
             currentURL = currentURL.deletingLastPathComponent()
 
             let packageURL = currentURL.appending(path: "Package.swift")
             if FileManager.default.fileExists(atPath: packageURL.path) {
-                return SourceBuildDirectories(source: packageURL.deletingLastPathComponent(),
-                                              build: packageURL.deletingLastPathComponent())
+                return SourceAndBuildDirectories(source: packageURL.deletingLastPathComponent(),
+                                                 build: packageURL.deletingLastPathComponent())
             }
         } while currentURL.path() !=  "/"
 
-        let buildDirectory: String = NSHomeDirectory() // app's home directory for a sandboxed MacOS app
-        if buildDirectory.contains("/Library/Containers/") {
-            let buildDirectoryURL: URL = URL(filePath: buildDirectory)
-            return SourceBuildDirectories(source: buildDirectoryURL,
-                                          build: buildDirectoryURL)
+        // assume we are building a sandboxed app that uses Ignite as a regular package
+        let buildDirectoryString = NSHomeDirectory() // app's home directory for a sandboxed MacOS app
+        let buildDirectoryURL = URL(filePath: buildDirectoryString)
+        let sourceDirectoryURL = buildDirectoryURL // has to contain any Assets/Content/Includes
+
+        guard buildDirectory.contains("/Library/Containers/") else {
+            throw PublishingError.missingPackageDirectory // actually
         }
 
-        throw PublishingError.missingPackageDirectory
+        return SourceAndBuildDirectories(source: sourceDirectoryURL,
+                                         build: buildDirectoryURL)
     }
 
 }
 
 /// Provides URL to where to find Assets/Content/Includes input directories and Build output directory
-public struct SourceBuildDirectories {
+public struct SourceAndBuildDirectories {
     public let source: URL
     public let build: URL
 }
