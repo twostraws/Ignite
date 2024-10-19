7fed55705d8d5604f687bc55451ffbac82a86320
Peter van den Hamer <13396568+vdhamer@users.noreply.github.com>
2024-10-19 21:32:27 +0200

    Fix: #29 Quite a struggle to have the FetchRequest depend on a passed parameter, but used Paul Hudson's trick of saving the query and not its result. Also some code restructuring.

--- Tests/IgniteTests/IgniteTests.swift
+++ Tests/IgniteTests/IgniteTests.swift
@@ -7,9 +7,9 @@ class ElementTest: XCTestCase {
 
     /// A publishing context with sample values for root site tests.
     let publishingContext = try! PublishingContext(for: TestSite(),
-                                                   rootURL: URL.documentsDirectory)
+                                                   from: URL.documentsDirectory)
     /// A publishing context with sample values for subsite tests.
     let publishingSubsiteContext = try! PublishingContext(for: TestSubsite(),
-                                                          rootURL: URL.documentsDirectory)
+                                                          from: URL.documentsDirectory)
 }
 // swiftlint:enable force_try
