d013757d1ecff39b08bc0f1c371b0f46690bc650
JPToroDev <jp@jptoro.dev>
2024-12-04 10:03:43 -0500

    Remove `@discardableResult` from `class()`.

--- Sources/Ignite/Framework/ElementTypes/HTML.swift
+++ Sources/Ignite/Framework/ElementTypes/HTML.swift
@@ -97,7 +97,7 @@ public extension HTML {
     /// Adds a single CSS class to the element.
     /// - Parameter className: The name of the CSS class to add
     /// - Returns: The modified HTML element
-    @discardableResult func `class`(_ className: String) -> Self {
+    func `class`(_ className: String) -> Self {
         var attributes = attributes
         attributes.classes.append(className)
         AttributeStore.default.merge(attributes, intoHTML: id)
