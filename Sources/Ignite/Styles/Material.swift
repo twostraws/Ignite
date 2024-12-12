56ff0ce37725231d7567430eeccfaaab9f0fab40
JPToroDev <jp@jptoro.dev>
2024-12-12 11:02:33 -0500

    Add SwiftUI-style `Material` type.

---
+++ Sources/Ignite/Styles/Material.swift
@@ -0,0 +1,77 @@
+//
+// Material.swift
+// Ignite
+// https://www.github.com/twostraws/Ignite
+// See LICENSE for license information.
+//
+
+/// A type that represents different material effects that can be applied to views.
+/// Material effects combine translucency, blur, and color to create the appearance
+/// of frosted glass. The effect automatically adapts to light and dark color schemes.
+public struct Material {
+    /// The type of material effect
+    private let type: MaterialType
+    /// Optional color scheme override
+    private let colorScheme: ColorScheme?
+
+    private enum MaterialType: String {
+        case ultraThin = "ultra-thin"
+        case thin = "thin"
+        case regular = "regular"
+        case thick = "thick"
+        case ultraThick = "ultra-thick"
+    }
+
+    /// The available color schemes for material effects
+    public enum ColorScheme: String {
+        case light
+        case dark
+    }
+
+    /// Creates a new material with the specified type
+    private init(type: MaterialType, colorScheme: ColorScheme? = nil) {
+        self.type = type
+        self.colorScheme = colorScheme
+    }
+
+    /// Method to override the color scheme
+    /// - Parameter scheme: The color scheme to use
+    /// - Returns: A new Material instance with the specified color scheme
+    public func colorScheme(_ scheme: ColorScheme) -> Material {
+        Material(type: self.type, colorScheme: scheme)
+    }
+
+    /// Gets the CSS class name for this material
+    fileprivate var className: String {
+        let baseClass = "material-\(type.rawValue)"
+        if let colorScheme {
+            return baseClass + "-" + colorScheme.rawValue
+        }
+        return baseClass
+    }
+
+    /// An ultra-thin material effect
+    @MainActor public static let ultraThinMaterial = Material(type: .ultraThin)
+
+    /// A thin material effect
+    @MainActor public static let thinMaterial = Material(type: .thin)
+
+    /// A regular material effect
+    @MainActor public static let regularMaterial = Material(type: .regular)
+
+    /// A thick material effect
+    @MainActor public static let thickMaterial = Material(type: .thick)
+
+    /// An ultra-thick material effect
+    @MainActor public static let ultraThickMaterial = Material(type: .ultraThick)
+}
+
+// Extension to allow materials to be used with background modifier
+public extension HTML {
+    /// Applies a material effect background
+    /// - Parameter material: The type of material to apply
+    /// - Returns: The modified HTML element
+    func background(_ material: Material) -> some HTML {
+        self.class(material.className)
+    }
+}
