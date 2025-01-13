4ad7a52ad15a2da7c6b0155c974e7fc7037fa3cb
Dorian Grolaux <6209874+MrSkwiggs@users.noreply.github.com>
2025-01-13 21:30:43 +0100

    Add tests for attributes order

---
+++ Tests/IgniteTesting/Elements/Attributes.swift
@@ -0,0 +1,40 @@
+//  Created by Dorian on 13/01/2025.
+//
+
+import Testing
+import Ignite
+
+/// Tests for the element's `Attributes`.
+@Suite("Attributes Tests")
+@MainActor
+struct AttributesTest {
+    let publishingContext = ElementTest.publishingContext
+
+    private nonisolated static let tags: [String] = ["body", "btn", "img", "div", "nav", "section"]
+
+
+    @Test("Checks that classes are sorted", arguments: tags)
+    func test_classes_are_sorted(tag: String) async throws {
+        let element = Tag(tag) {}.class("foo", "bar", "baz", "qux")
+        let output = element.render(context: publishingContext)
+        let expected = "<\(tag) class=\"bar baz foo qux\"></\(tag)>"
+
+        #expect(
+            output == expected
+        )
+    }
+
+    @Test("Checks that custom attributes are sorted", arguments: Self.tags)
+    func test_custom_attributes_are_sorted(tag: String) async throws {
+        let element = Tag(tag) {}
+            .customAttribute(name: "qux", value: "qux")
+            .customAttribute(name: "baz", value: "baz")
+            .customAttribute(name: "foo", value: "foo")
+            .customAttribute(name: "bar", value: "bar")
+        let output = element.render(context: publishingContext)
+
+        #expect(
+            output == "<\(tag) bar=\"bar\" baz=\"baz\" foo=\"foo\" qux=\"qux\"></\(tag)>"
+        )
+    }
+}
