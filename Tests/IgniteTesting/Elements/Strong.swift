a89f39694740b950e34e9796d3742bd4b26cf84d
Gemma Barlow <gemmakbarlow@gmail.com>
2025-01-16 15:41:52 -0500

    Add tests for Strong element
    Almost identical to those in the project for Span

---
+++ Tests/IgniteTesting/Elements/Strong.swift
@@ -0,0 +1,33 @@
+//
+// Strong.swift
+// Ignite
+// https://www.github.com/twostraws/Ignite
+// See LICENSE for license information.
+//
+
+import Foundation
+import Testing
+
+@testable import Ignite
+
+/// Tests for the `Strong` element.
+@Suite("Strong Tests")
+@MainActor struct StrongTests {
+    let publishingContext = ElementTest.publishingContext
+
+    @Test("Single Element Test", arguments: ["This is a test", "Another test"])
+    func test_singleElement(strongText: String) async throws {
+        let element = Strong(strongText)
+        let output = element.render(context: publishingContext)
+
+		#expect(output == "<strong>\(strongText)</strong>")
+    }
+
+    @Test("Builder Test", arguments: ["This is a test", "Another test"])
+    func test_builder(strongText: String) async throws {
+        let element = Strong { strongText }
+        let output = element.render(context: publishingContext)
+
+        #expect(output == "<strong>\(strongText)</strong>")
+    }
+}
