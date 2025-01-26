a180108bae3379aeaf6cf0b78628921e95f6957c
Rimesh Jotaniya <rimesh.j@icloud.com>
2025-01-27 00:18:05 +0530

    Added tests for VStack

--- Tests/IgniteTesting/Elements/VStack.swift
+++ Tests/IgniteTesting/Elements/VStack.swift
@@ -14,8 +14,37 @@ import Testing
 @Suite("VStack Tests")
 @MainActor
 struct VStackTests {
-    @Test("ExampleTest")
-    func example() async throws {
+    @Test("VStack with elements")
+    func basicVStack() async throws {
+        let element = VStack {
+            Label(text: "Top Label")
+            Label(text: "Bottom Label")
+        }
+        let output = element.render()
 
+        #expect(output == """
+        <div class="vstack">\
+        <label class="mb-0">Top Label</label>\
+        <label class="mb-0">Bottom Label</label>\
+        </div>
+        """
+        )
+    }
+
+    @Test("VStack with elements with spacing")
+    func elementsWithSpacingWithinVStack() async throws {
+        let element = VStack(spacing: 10) {
+            Label(text: "Top Label")
+            Label(text: "Bottom Label")
+        }
+        let output = element.render()
+
+        #expect(output == """
+        <div class="vstack" style="gap: 10px">\
+        <label class="mb-0">Top Label</label>\
+        <label class="mb-0">Bottom Label</label>\
+        </div>
+        """
+        )
     }
 }
