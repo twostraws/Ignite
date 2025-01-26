803452ccb22a79794be470bc8768278a020a5c37
Andrew Erickson <aerickson14@gmail.com>
2025-01-26 11:39:10 -0700

    add some initial tests for markdown renderer

--- Tests/IgniteTesting/Framework/MarkdownRenderer.swift
+++ Tests/IgniteTesting/Framework/MarkdownRenderer.swift
@@ -14,8 +14,42 @@ import Testing
 @Suite("MarkdownRenderer Tests")
 @MainActor
 struct MarkdownRendererTests {
-    @Test("ExampleTest")
-    func example() async throws {
 
+    @Test("Markdown headers from string", arguments: ["# Header 1", "## Header 2", "### Header 3"])
+    func convertHeadersToHTML(markdown: String) async throws {
+        let element = MarkdownToHTML(markdown: markdown, removeTitleFromBody: false)
+
+        let expectedTag = "h\(numberOfHashtags(in: markdown))"
+        let expectedContent = markdown
+            .replacingOccurrences(of: "#", with: "")
+            .trimmingCharacters(in: .whitespacesAndNewlines)
+        #expect(element.body == "<\(expectedTag)>\(expectedContent)</\(expectedTag)>")
+    }
+
+    @Test("Markdown paragraphs from string", arguments: ["Paragraph one\n\nParagraph two"])
+    func convertParagraphsToHTML(markdown: String) async throws {
+        let element = MarkdownToHTML(markdown: markdown, removeTitleFromBody: false)
+
+        let paragraphs = markdown.split(separator: "\n\n")
+        let expectedHTML = paragraphs
+            .map {
+                "<p>\($0)</p>"
+            }
+            .joined()
+        #expect(element.body == "\(expectedHTML)")
+    }
+
+    @Test("Markdown block quotes from string", arguments: ["> Here is an example quote"])
+    func convertBlockQuotesToHTML(markdown: String) async throws {
+        let element = MarkdownToHTML(markdown: markdown, removeTitleFromBody: false)
+
+        let content = markdown.replacingOccurrences(of: "> ", with: "")
+        #expect(element.body == "<blockquote><p>\(content)</p></blockquote>")
+    }
+}
+
+extension MarkdownRendererTests {
+    private func numberOfHashtags(in markdown: String) -> Int {
+        return markdown.filter { $0 == "#" }.count
     }
 }
