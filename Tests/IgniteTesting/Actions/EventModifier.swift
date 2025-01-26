1ffd8e8f5ab96345e772f82aa042330d0a26aab3
Vishal Kanojia <vishal.deep@live.com>
2025-01-26 14:35:57 -0800

    Add tests for EventModifier

--- Tests/IgniteTesting/Actions/EventModifier.swift
+++ Tests/IgniteTesting/Actions/EventModifier.swift
@@ -14,8 +14,36 @@ import Testing
 @Suite("EventModifier Tests")
 @MainActor
 struct EventModifierTests {
-    @Test("ExampleTest")
-    func example() async throws {
+    private nonisolated static let tags: [String] = ["body", "btn", "img", "div", "section"]
 
+    private static let events: [EventType] = [
+        EventType.click,
+        EventType.doubleClick,
+        EventType.focus,
+        EventType.load,
+        EventType.mouseOver
+    ]
+
+    private static let actions: [[any Action]] = [
+        [ShowAlert(message: "foo")],
+        [ShowAlert(message: "qux"), DismissModal(id: "baz")],
+        [CustomAction("document.writeline('bar')")],
+        [CustomAction("document.writeline('foo')"), ShowElement("qux")],
+        [ShowModal(id: "foo"), HideElement("qux")]
+    ]
+
+    @Test("Check if events are added correctly", arguments: tags)
+    func onEventAddsEventsCorrectly(tag: String) async throws {
+        zip(Self.events, Self.actions).forEach { event, actions in
+            let element = Tag(tag) {}
+                .onEvent(event, actions)
+
+            let output = element.render()
+
+            let eventOutput = event.rawValue
+            let actionOutput = actions.map { $0.compile() }.joined(separator: "; ")
+
+            #expect(output == "<\(tag) \(eventOutput)=\"\(actionOutput)\"></\(tag)>")
+        }
     }
 }
