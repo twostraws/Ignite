00899ae5955ac4a21d83c8ad69abcf4c1f9c39a0
Andrew Erickson <aerickson14@gmail.com>
2025-01-26 12:25:08 -0700

    add more markdown tests

--- Tests/IgniteTesting/Framework/MarkdownRenderer.swift
+++ Tests/IgniteTesting/Framework/MarkdownRenderer.swift
@@ -48,6 +48,87 @@ struct MarkdownRendererTests {
         let content = markdown.replacingOccurrences(of: "> ", with: "")
         #expect(element.body == "<blockquote><p>\(content)</p></blockquote>")
     }
+
+    @Test("Markdown image from string", arguments: ["Here is an ![Image description](path/to/example/image.jpg)"])
+    func convertImageToHTML(markdown: String) async throws {
+        let element = MarkdownToHTML(markdown: markdown, removeTitleFromBody: false)
+
+        let expectedImageHTML = "<img src=\"path/to/example/image.jpg\" alt=\"Image description\" class=\"img-fluid\">"
+        #expect(element.body == "<p>Here is an \(expectedImageHTML)</p>")
+    }
+
+    @Test("Markdown code block from string", arguments: ["Here is some `var code = \"great\"`"])
+    func convertCodeBlockToHTML(markdown: String) async throws {
+        let element = MarkdownToHTML(markdown: markdown, removeTitleFromBody: false)
+
+        #expect(element.body == "<p>Here is some <code>var code = \"great\"</code></p>")
+    }
+
+    @Test("Markdown emphasis from string", arguments: ["Here is some *emphasized* text"])
+    func convertEmphasisToHTML(markdown: String) async throws {
+        let element = MarkdownToHTML(markdown: markdown, removeTitleFromBody: false)
+
+        #expect(element.body == "<p>Here is some <em>emphasized</em> text</p>")
+    }
+
+    @Test("Markdown link from string", arguments: ["Here is a [link](https://example.com)"])
+    func convertLinkToHTML(markdown: String) async throws {
+        let element = MarkdownToHTML(markdown: markdown, removeTitleFromBody: false)
+
+        #expect(element.body == "<p>Here is a <a href=\"https://example.com\">link</a></p>")
+    }
+
+    @Test("Markdown list from string")
+    func convertListToHTML() async throws {
+        let markdown = """
+- Item 1
+- Item 2
+"""
+        let element = MarkdownToHTML(markdown: markdown, removeTitleFromBody: false)
+
+        #expect(element.body == "<ul><li>Item 1</li><li>Item 2</li></ul>")
+    }
+
+    @Test("Markdown ordered list from string")
+    func convertOrderedListToHTML() async throws {
+        let markdown = """
+1. Item 1
+2. Item 2
+"""
+        let element = MarkdownToHTML(markdown: markdown, removeTitleFromBody: false)
+
+        #expect(element.body == "<ol><li>Item 1</li><li>Item 2</li></ol>")
+    }
+
+    @Test("Markdown strikethrough from string")
+    func convertStrikethroughToHTML() async throws {
+        let markdown = "Example text with some of it ~crossed out~"
+        let element = MarkdownToHTML(markdown: markdown, removeTitleFromBody: false)
+
+        #expect(element.body == "<p>Example text with some of it <s>crossed out</s></p>")
+    }
+
+    @Test("Markdown strong from string")
+    func convertStrongToHTML() async throws {
+        let markdown = "Example of **strong** text"
+        let element = MarkdownToHTML(markdown: markdown, removeTitleFromBody: false)
+
+        #expect(element.body == "<p>Example of <strong>strong</strong> text</p>")
+    }
+
+    @Test("Markdown thematic break from string")
+    func convertThematicBreakToHTML() async throws {
+        let markdown = """
+Text above
+
+---
+
+Text below
+"""
+        let element = MarkdownToHTML(markdown: markdown, removeTitleFromBody: false)
+
+        #expect(element.body == "<p>Text above</p><hr /><p>Text below</p>")
+    }
 }
 
 extension MarkdownRendererTests {
