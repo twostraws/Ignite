47938efa8c212ff05614cf518b78d206f83ad696
Dorian Grolaux <6209874+MrSkwiggs@users.noreply.github.com>
2025-01-27 17:07:35 +0100

    Add new action

---
+++ Sources/Ignite/Actions/ToggleElementVisibility.swift
@@ -0,0 +1,25 @@
+//
+//  ToggleElementVisiblity.swift
+//  Ignite
+//
+//  Created by Dorian on 27/01/2025.
+//
+
+import Foundation
+
+/// Toggles an element's visibility by adding or removing the "d-none" CSS class.
+public struct ToggleElementVisibility: Action {
+    /// The unique identifier of the element we're trying to hide.
+    var id: String
+
+    /// Creates a new ShowElement action from a specific page element ID.
+    /// - Parameter id: The unique identifier of the element we're trying to hide.
+    public init(_ id: String) {
+        self.id = id
+    }
+
+    public func compile() -> String {
+        "document.getElementById('\(id)').classList.toggle('d-none')"
+    }
+}
+
