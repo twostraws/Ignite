e33ddd95e276497153ed920d82c7878bbe76ed8c
Joseph Wardell <jay@jaywardell.me>
2025-01-26 22:29:05 -0500

    added a test for noop cases

--- Tests/IgniteTesting/Extensions/String-KebabCased.swift
+++ Tests/IgniteTesting/Extensions/String-KebabCased.swift
@@ -14,8 +14,19 @@ import Testing
 @Suite("String-KebabCased Tests")
 @MainActor
 struct StringKebabCasedTests {
-    @Test("ExampleTest")
-    func example() async throws {
-
+    
+    
+    /// Some types of string will have an output that's the same as their input.
+    /// Test examples of each of those cases
+    @Test("Noop Cases", arguments: [
+        "", // empty string
+        "a", // single character
+        "1", // single digit
+        "one", // single word
+        "hello_world", // words connected by underscores
+        "hello_happy_world" // words connected by underscores
+    ])
+    func does_not_change_simple_cases(string: String) async throws {
+        #expect(string.kebabCased() == string)
     }
 }
