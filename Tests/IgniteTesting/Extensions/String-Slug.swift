87e8fa51e363db6a04c0b126c5586ebe8dc85d72
Joseph Wardell <jay@jaywardell.me>
2025-01-27 12:28:17 -0500

    added a test to ensure that non-latin scripts are transliterated into latin characters

--- Tests/IgniteTesting/Extensions/String-Slug.swift
+++ Tests/IgniteTesting/Extensions/String-Slug.swift
@@ -31,5 +31,28 @@ struct StringSlugTests {
     func returns_nil_for_strings_with_no_latin_characters(string: String) async throws {
         #expect(string.convertedToSlug() == nil)
     }
+    
+    struct Instance {
+        let input: String
+        let expected: String
+    }
+
+    @Test("Transliterates Non-latin Scripts", arguments: [
+        // I've tried to use non-controversial words here
+        // "peace" in two of the scripts
+        // and "oolong tea" in the other two.
+        // these were also just very easy-to-find words online
+        // as I don't speak any of these languages.
+        // No stereotyping or cultural judgment is intended
+        Instance(input: "烏龍茶", expected: "wu-long-cha"),
+        Instance(input: "ウーロン茶", expected: "uron-cha"),
+        Instance(input: "שָׁלוֹם", expected: "salwom"),
+        Instance(input: "שָׁלוֹםسلام", expected: "salwomslam"),
+        Instance(input: "שָׁלוֹםسلام" + "שָׁלוֹם" + "烏龍茶" + "ウーロン茶", expected: "salwomslamsalwom-wu-long-chauron-cha"),
+
+    ])
+    func transliterates_characters_in_non_latin_scripts(instance: Instance) async throws {
+        #expect(instance.input.convertedToSlug() == instance.expected)
+    }
 
 }
