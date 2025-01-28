aa37c1fd8e869e5473ecd3479cacfdba1edde92b
Joseph Wardell <jay@jaywardell.me>
2025-01-28 12:47:28 -0500

    added a timeZone property to Site. It defaults to nil

--- Sources/Ignite/Framework/Site.swift
+++ Sources/Ignite/Framework/Site.swift
@@ -71,6 +71,10 @@ public protocol Site: Sendable {
     /// The base URL for your site, e.g. https://www.example.com
     var url: URL { get }
 
+    /// the time zone used to do date output for your site. Defaults to nil
+    /// if nil, then date output will be assume a GMT time zone
+    var timeZone: TimeZone? { get }
+
     /// Choose whether to use a local version of Bootstrap, a remote version,
     /// or none at all
     var useDefaultBootstrapURLs: BootstrapOptions { get }
@@ -148,6 +152,10 @@ public extension Site {
 
     /// English as default language.
     var language: Language { .english }
+
+    /// by default, time zone is nil
+    /// which means that date output is considered to be GMT
+    var timeZone: TimeZone? { nil }
 
     /// Uses the default light theme based on Bootstrap.
     var lightTheme: (any Theme)? { DefaultLightTheme() }
