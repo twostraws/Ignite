640df33f25ed59d97945cc0a4a4e748fbeb19ded
Nigel Gee <41487836+NigelGee@users.noreply.github.com>
2025-01-28 21:09:39 +0000

    Added Test for ForegroundStyle

--- Tests/IgniteTesting/Modifiers/ForegroundStyle.swift
+++ Tests/IgniteTesting/Modifiers/ForegroundStyle.swift
@@ -14,8 +14,51 @@ import Testing
 @Suite("ForegroundStyle Tests")
 @MainActor
 struct ForegroundStyleTests {
-    @Test("ExampleTest")
-    func example() async throws {
+    @Test("Common foreground styles Test", arguments: ForegroundStyle.allCases)
+    func commonForegroundStyle(style: ForegroundStyle) async throws {
+        let element = Text("Hello").foregroundStyle(style)
 
+        let output = element.render()
+
+        #expect(output == "<p class=\"\(style.rawValue)\">Hello</p>")
+    }
+
+    @Test("Color foreground styles Test", arguments:
+            zip(
+                [Color.white,
+                 .black,
+                 .red,
+                 .green,
+                 .blue
+                ],
+                [
+                 "rgb(255 255 255 / 100%)",
+                 "rgb(0 0 0 / 100%)",
+                 "rgb(255 0 0 / 100%)",
+                 "rgb(0 128 0 / 100%)",
+                 "rgb(0 0 255 / 100%)"
+                ]
+               )
+    )
+          func colorForegroundStyle(color: Color, value: String) async throws {
+        let element = Text("Hello").foregroundStyle(color)
+
+        let output = element.render()
+
+        #expect(output == "<p style=\"color: \(value)\">Hello</p>")
+    }
+
+    @Test("Color foreground styles Test", arguments: ["white",
+                                                      "black",
+                                                      "red",
+                                                      "blue"
+                                                     ]
+    )
+          func stringForegroundStyle(string: String) async throws {
+        let element = Text("Hello").foregroundStyle(string)
+
+        let output = element.render()
+
+              #expect(output == "<p style=\"color: \(string)\">Hello</p>")
     }
 }
