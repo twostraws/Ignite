b070a0c7fa7f5cbb368ce10db9956a9045c110be
JP Toro <jp@jptoro.dev>
2025-01-28 12:25:55 -0500

    Update `AnimationModifier` test.

--- Tests/IgniteTesting/Modifiers/AnimationModifier.swift
+++ Tests/IgniteTesting/Modifiers/AnimationModifier.swift
@@ -30,10 +30,10 @@ struct AnimationModifierTests {
         // <div class="animation-H57c-transform" style="transform-style: preserve-3d">
         // <div class="animation-H57c-hover"><p><span>This is a Span</span></p></div></div>
         // The 'H57c' is a random element, that varies at runtime but should be the same for both divs
-        // (?<code>[a-zA-Z0-9]{4}) matches that and names it `code`; \k<code> re-uses that value
+        // (?<code>[a-zA-Z0-9]{5}) matches that and names it `code`; \k<code> re-uses that value
         // to ensure that the same random part is used in both div's
         let pattern = #"""
-            <div class="animation-(?<code>[a-zA-Z0-9]{4})-transform" style="transform-style: preserve-3d">\
+            <div class="animation-(?<code>[a-zA-Z0-9]{5})-transform" style="transform-style: preserve-3d">\
             <div class="animation-\k<code>-hover">\
             <p><span>This is a Span</span></p>\
             </div></div>
