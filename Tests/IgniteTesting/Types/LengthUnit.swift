f218c5dc1e3cb0d31740c28810700f1ff0a25c49
mauricebonnes <bonnes@hs-mittweida.de>
2025-01-29 12:07:52 +0100

    Add length unit tests

--- Tests/IgniteTesting/Types/LengthUnit.swift
+++ Tests/IgniteTesting/Types/LengthUnit.swift
@@ -14,8 +14,80 @@ import Testing
 @Suite("LengthUnit Tests")
 @MainActor
 struct LengthUnitTests {
-    @Test("ExampleTest")
-    func example() async throws {
+    @Test("Test pixels length unit.", arguments: [
+        10,
+        25,
+        152
+    ])
+    func pixels(pixelAmount: Int) async throws {
+        let element = LengthUnit.px(pixelAmount)
 
+        #expect(element.description == "\(pixelAmount)px")
+    }
+
+    @Test("Test rem length unit.", arguments: [
+        10,
+        25,
+        152
+    ])
+    func rem(pixelAmount: Double) async throws {
+        let element = LengthUnit.rem(pixelAmount)
+
+        #expect(element.description == "\(pixelAmount)rem")
+    }
+
+    @Test("Test em length unit.", arguments: [
+        10,
+        25,
+        152
+    ])
+    func em(pixelAmount: Double) async throws {
+        let element = LengthUnit.em(pixelAmount)
+
+        #expect(element.description == "\(pixelAmount)em")
+    }
+
+    @Test("Test percentage length unit.", arguments: [
+        Percentage(10),
+        Percentage(25),
+        Percentage(-67)
+    ])
+    func percentage(percent: Percentage) async throws {
+        let element = LengthUnit.percent(percent)
+
+        #expect(element.description == "\(percent.value)%")
+    }
+
+    @Test("Test vw length unit.", arguments: [
+        Percentage(10),
+        Percentage(25),
+        Percentage(67)
+    ])
+    func vw(percent: Percentage) async throws {
+        let element = LengthUnit.vw(percent)
+
+        #expect(element.description == "\(percent.value)vw")
+    }
+
+    @Test("Test vh length unit.", arguments: [
+        Percentage(10),
+        Percentage(25),
+        Percentage(67)
+    ])
+    func vh(percent: Percentage) async throws {
+        let element = LengthUnit.vh(percent)
+
+        #expect(element.description == "\(percent.value)vh")
+    }
+
+    @Test("Test custom length unit.", arguments: [
+        "60vw",
+        "300px",
+        "25%"
+    ])
+    func custom(unit: String) async throws {
+        let element = LengthUnit.custom(unit)
+
+        #expect(element.description == unit)
     }
 }
