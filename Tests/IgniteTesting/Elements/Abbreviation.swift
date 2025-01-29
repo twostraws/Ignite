f019e1f49bd4097518574ad254003b9df2f2e8a8
JP Toro <jp@jptoro.dev>
2025-01-28 20:49:49 -0500

    Fix linting error.

--- Tests/IgniteTesting/Elements/Abbreviation.swift
+++ Tests/IgniteTesting/Elements/Abbreviation.swift
@@ -18,22 +18,19 @@ class AbbreviationTests: IgniteSuite {
     func basic(abbreviation: String, description: String) async throws {
         let element = Abbreviation(abbreviation, description: description)
         let output = element.render()
-
-        #expect(
-            output == "<abbr title=\"\(description)\">\(abbreviation)</abbr>")
+        #expect(output == "<abbr title=\"\(description)\">\(abbreviation)</abbr>")
     }
 
     @Test("Single Element Abbreviation Test", arguments: ["abbreviation"], ["abbr"])
-    func singleElement(description: String, abbreviation: String)
-        async throws
-    {
+    func singleElement(description: String, abbreviation: String) async throws {
         let element = Abbreviation(description) { Strong(abbreviation) }
         let output = element.render()
 
-        #expect(
-            output
-                == "<abbr title=\"\(description)\"><strong>\(abbreviation)</strong></abbr>"
-        )
+        #expect(output == """
+        <abbr title=\"\(description)\">\
+        <strong>\(abbreviation)</strong>\
+        </abbr>
+        """)
     }
 
     @Test("Builder Abbreviation Test", arguments: ["abbreviation"], ["abbr"])
@@ -46,9 +43,10 @@ class AbbreviationTests: IgniteSuite {
 
         let output = element.render()
 
-        #expect(
-            output
-                == "<abbr title=\"\(description)\"><strong>\(abbreviation)</strong></abbr>"
-        )
+        #expect(output == """
+        "<abbr title=\"\(description)\">\
+        <strong>\(abbreviation)</strong>\
+        </abbr>
+        """)
     }
 }
