3ee29d06aa2e8c6aed8d3edba94a54352da5a7e8
5385825+bobdel@users.noreply.github.com <5385825+bobdel@users.noreply.github.com>
2025-01-29 14:30:16 -0800

    Refined the Color test suite from the intial attempt made during the workshop.

--- Tests/IgniteTesting/Types/Color.swift
+++ Tests/IgniteTesting/Types/Color.swift
@@ -11,21 +11,46 @@ import Testing
 @testable import Ignite
 
 /// Tests for the `Color` type.
-@Suite("Color Tests")
+@Suite("Color Type Tests")
 @MainActor
-struct ColorTests {
-    @Test("foregroundStyleIsBlack")
+struct ColorTypeTests {
+    @Test("black and white via parameters",
+          arguments: zip(
+            [Color.black, .white],
+            ["0 0 0", "255 255 255"]
+          )
+    )
+    func makeColor(forgroundStyle: Color, rgbValues: String) async throws {
+        let coloredText = Text("Hello, world!")
+            .foregroundStyle(forgroundStyle)
 
-    func foregroundStyleIsBlack() async throws {
-        let foregroundStyleIsBlack = Text("Hello, world!")
-            .foregroundStyle(.black)
-
-        let output = foregroundStyleIsBlack.render()
+        let output = coloredText.render()
 
         #expect(output ==
         """
-        <p style="color: rgb(0 0 0 / 100%)">Hello, world!</p>
+        <p style="color: rgb(\(rgbValues) / 100%)">Hello, world!</p>
         """
         )
     }
+
+    @Test("Initialize Color with Int values")
+    func testInitializeWithIntValues() async throws {
+        let color = Color(red: 255, green: 0, blue: 0)
+        #expect(color == .red)
+        #expect(color.red == 255)
+        #expect(color.green == 0)
+        #expect(color.blue == 0)
+    }
+
+    @Test("Return CSS value of color")
+    func testReturnCSSValueOfColor() async throws {
+        let red = 255
+        let green = 0
+        let blue = 0
+
+        let color = Color(red: red, green: green, blue: blue)
+        #expect(color.description == "rgb(\(red) \(blue) \(green) / 100%)")
+    }
+
+    // TODO: test opacity and remove use of foregroundStyle
 }
