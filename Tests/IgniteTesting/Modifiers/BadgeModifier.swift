9685c078c3595022b4a88b7502389bfa85c08ed8
Michael Murphy <MurphyCorona@yahoo.com>
2025-01-29 19:53:25 -0700

    Corrected linting violation in BadgeModifierTests

--- Tests/IgniteTesting/Modifiers/BadgeModifier.swift
+++ Tests/IgniteTesting/Modifiers/BadgeModifier.swift
@@ -18,7 +18,10 @@ struct BadgeModifierTests {
     func badgeModifierForInlineHTML() async throws {
         let element = Text("Notifications").badge(Badge("3"))
         let output = element.render()
-        #expect(output == "<li class=\"align-items-center d-flex justify-content-between\"><p>Notifications</p><span class=\"badge rounded-pill\">3</span></li>")
+        #expect(
+            output == "<li class=\"align-items-center d-flex justify-content-between\">" +
+            "<p>Notifications</p><span class=\"badge rounded-pill\">3</span></li>"
+        )
     }
 
     @Test("Badge Modifier for ListItem")
@@ -27,6 +30,9 @@ struct BadgeModifierTests {
             Text("Messages")
         }.badge(Badge("5"))
         let output = element.render()
-        #expect(output == "<li class=\"align-items-center d-flex justify-content-between\"><p>Messages</p></li>")
+        #expect(
+            output == "<li class=\"align-items-center d-flex justify-content-between\">" +
+            "<p>Messages</p></li>"
+        )
     }
 }
