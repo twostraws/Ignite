9384b025a9466569ef88424678d9e7824efcb0cf
mauricebonnes <bonnes@hs-mittweida.de>
2025-01-30 13:23:43 +0100

    Add tests for inlineStyles

--- Tests/IgniteTesting/Types/InlineStyle.swift
+++ Tests/IgniteTesting/Types/InlineStyle.swift
@@ -14,8 +14,28 @@ import Testing
 @Suite("InlineStyle Tests")
 @MainActor
 struct InlineStyleTests {
-    @Test("ExampleTest")
-    func example() async throws {
+    @Test("Test correct description for property string initializer.")
+    func descriptionPropertyStringInit() async throws {
+        let example = InlineStyle("font-size", value: "25px")
 
+        #expect(example.description == "font-size: 25px")
+    }
+
+    @Test("Test correct description for property initializer.")
+    func descriptionPropertyInit() async throws {
+        let example = InlineStyle(Property.fontSize, value: "25px")
+
+        #expect(example.description == "font-size: 25px")
+    }
+
+    @Test("Test comparable operator.", arguments: [
+        (InlineStyle(.absolutePosition, value: "top"), InlineStyle(.accentColor, value: "red"), true),
+        (InlineStyle(.backgroundColor, value: "red"), InlineStyle(.absolutePosition, value: "top"), false),
+        (InlineStyle(.textAlign, value: "center"), InlineStyle(.color, value: "red"), false),
+        (InlineStyle(.justifyContent, value: "space-between"), InlineStyle(.alignItems, value: "end"), false)
+    ])
+    func comparable(lhs: InlineStyle, rhs: InlineStyle, expected: Bool) async throws {
+        #expect((lhs < rhs) == expected)
+        #expect((lhs > rhs) == !expected)
     }
 }
