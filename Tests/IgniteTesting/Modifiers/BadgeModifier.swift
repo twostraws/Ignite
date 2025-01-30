02bcf0251c5b6271f8ef45084aecf6f6f1947235
Michael Murphy <MurphyCorona@yahoo.com>
2025-01-29 19:24:11 -0700

    Added a test for BadgeModifier

--- Tests/IgniteTesting/Modifiers/BadgeModifier.swift
+++ Tests/IgniteTesting/Modifiers/BadgeModifier.swift
@@ -14,8 +14,19 @@ import Testing
 @Suite("BadgeModifier Tests")
 @MainActor
 struct BadgeModifierTests {
-    @Test("ExampleTest")
-    func example() async throws {
+    @Test("Badge Modifier for InlineHTML")
+    func badgeModifierForInlineHTML() async throws {
+        let element = Text("Notifications").badge(Badge("3"))
+        let output = element.render()
+        #expect(output == "<li class=\"align-items-center d-flex justify-content-between\"><p>Notifications</p><span class=\"badge rounded-pill\">3</span></li>")
+    }
 
+    @Test("Badge Modifier for ListItem")
+    func badgeModifierForListItem() async throws {
+        let element = ListItem {
+            Text("Messages")
+        }.badge(Badge("5"))
+        let output = element.render()
+        #expect(output == "<li class=\"align-items-center d-flex justify-content-between\"><p>Messages</p></li>")
     }
 }
