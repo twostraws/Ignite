21cc0717e0d5902aaef808e067fff5a326078d10
rauhja <82282638+rauhja@users.noreply.github.com>
2025-01-31 16:27:10 +0200

    Added a test for LineSpacing modifier

--- Tests/IgniteTesting/Modifiers/LineSpacing.swift
+++ Tests/IgniteTesting/Modifiers/LineSpacing.swift
@@ -14,8 +14,47 @@ import Testing
 @Suite("LineSpacing Tests")
 @MainActor
 struct LineSpacingTests {
-    @Test("ExampleTest")
-    func example() async throws {
+    @Test("LineSpacing with custom line height", arguments: [
+        (value: 2.5, expected: "2.5"),
+        (value: 0.0, expected: "0"),
+        (value: -2.0, expected: "-2")
+    ])
+    func textWithCustomLineSpacing(testCase: (value: Double, expected: String)) async throws {
+        let element = Text("Hello, world!").lineSpacing(testCase.value)
+        let output = element.render()
 
+        #expect(output == "<p style=\"line-height: \(testCase.expected)\">Hello, world!</p>")
+    }
+
+    @Test("LineSpacing with xSmall preset line height")
+    func xSmallPresetLineSpacing() async throws {
+        let element = Text("Hello, world!").lineSpacing(.xSmall)
+        let output = element.render()
+
+        #expect(output == "<p class=\"lh-1\">Hello, world!</p>")
+    }
+
+    @Test("LineSpacing with small preset line height")
+    func smallPresetLineSpacing() async throws {
+        let element = Text("Hello, world!").lineSpacing(.small)
+        let output = element.render()
+
+        #expect(output == "<p class=\"lh-sm\">Hello, world!</p>")
+    }
+
+    @Test("LineSpacing with small preset line height")
+    func standardPresetLineSpacing() async throws {
+        let element = Text("Hello, world!").lineSpacing(.standard)
+        let output = element.render()
+
+        #expect(output == "<p class=\"lh-base\">Hello, world!</p>")
+    }
+
+    @Test("LineSpacing with small preset line height")
+    func largePresetLineSpacing() async throws {
+        let element = Text("Hello, world!").lineSpacing(.large)
+        let output = element.render()
+
+        #expect(output == "<p class=\"lh-lg\">Hello, world!</p>")
     }
 }
