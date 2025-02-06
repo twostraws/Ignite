e07cc1d25a2e45ac2cce03cbfee3c57f30f422a2
Joseph Wardell <jay@jaywardell.me>
2025-02-05 19:18:13 -0500

    moved the test of MetaLink.highlighterThemeMetaLinks() from  HTMLHeadTests to a separate Suite for testing MetaLink.highlighterThemeMetaLinks

--- Tests/IgniteTesting/Elements/MetaLink.swift
+++ Tests/IgniteTesting/Elements/MetaLink.swift
@@ -48,3 +48,25 @@ struct MetaLinkTests {
         #expect(output == "<link href=\"https://www.example.com\" rel=\"alternate\" />")
     }
 }
+
+@Suite("MetaLink.highlighterThemeMetaLinks Tests")
+@MainActor
+struct highlighterThemeMetaLinksTests {
+    init() throws {
+        try PublishingContext.initialize(for: TestSite(), from: #filePath)
+    }
+
+    @Test("Highlighting meta tags are sorted")
+    func highlighterThemesAreSorted() async throws {
+        let links = MetaLink.highlighterThemeMetaLinks(for: [.xcodeDark, .githubDark, .twilight])
+        let output = links.map { $0.render() }
+
+        #expect(
+            output == [
+                "<link href=\"/css/prism-github-dark.css\" rel=\"stylesheet\" data-highlight-theme=\"github-dark\" />",
+                "<link href=\"/css/prism-twilight.css\" rel=\"stylesheet\" data-highlight-theme=\"twilight\" />",
+                "<link href=\"/css/prism-xcode-dark.css\" rel=\"stylesheet\" data-highlight-theme=\"xcode-dark\" />"
+            ]
+        )
+    }
+}
