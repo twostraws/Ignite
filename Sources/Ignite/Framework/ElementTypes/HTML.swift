983d514c01cbb4410d84ae7e9fa5aa088815847f
JP Toro <jp@jptoro.dev>
2025-02-06 20:31:13 -0500

    Fix associated type names.

--- Sources/Ignite/Framework/ElementTypes/HTML.swift
+++ Sources/Ignite/Framework/ElementTypes/HTML.swift
@@ -22,10 +22,10 @@ public protocol HTML: CustomStringConvertible, Sendable {
     var isPrimitive: Bool { get }
 
     /// The type of HTML content this element contains.
-    associatedtype Content: HTML
+    associatedtype Body: HTML
 
     /// The content and behavior of this `HTML` element.
-    @HTMLBuilder var body: Content { get }
+    @HTMLBuilder var body: Body { get }
 
     /// Converts this element and its children into an HTML string with attributes.
     /// - Returns: A string containing the rendered HTML
--- Sources/Ignite/Framework/Layout.swift
+++ Sources/Ignite/Framework/Layout.swift
@@ -23,10 +23,10 @@
 @MainActor
 public protocol Layout {
     /// The type of HTML content this layout will generate
-    associatedtype Body: HTML
+    associatedtype Content: HTML
 
     /// The main content of the layout, built using the HTML DSL
-    @LayoutBuilder var body: Body { get }
+    @LayoutBuilder var body: Content { get }
 
     /// A unique identifier for this layout instance
     var id: String { get set }
