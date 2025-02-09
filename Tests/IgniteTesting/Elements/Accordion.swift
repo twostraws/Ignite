5e819036fa7edb903f64c9874eec0994b6411f20
Joseph Wardell <jay@jaywardell.me>
2025-02-08 21:15:14 -0500

    added a test to ensure that the id of the div tag is unique and starts with the word "accordion"

--- Tests/IgniteTesting/Elements/Accordion.swift
+++ Tests/IgniteTesting/Elements/Accordion.swift
@@ -22,8 +22,22 @@ struct AccordionTests {
         let attributes = try #require(sut.render().htmlTagWithCloseTag("div")?.attributes)
         let classAttribute = try #require(attributes.htmlAttribute(named: "class"))
         
-        #expect(clss == "accordion")
+        #expect(classAttribute == "accordion")
     }
+    
+    @Test("Provides a Unique id")
+    func outputs_div_with_unique_id() async throws {
+        let sut = Accordion {}
+
+        let idattribute = try #require(sut.render().htmlTagWithCloseTag("div")?
+            .attributes
+            .htmlAttribute(named: "id")
+        )
+
+        let expected = /accordion[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]/
+        #expect(nil != idattribute.firstMatch(of: expected))
+    }
+
     
     @Test("ExampleTest")
     func print_example() async throws {
