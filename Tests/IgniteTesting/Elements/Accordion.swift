51781630ab0c705e0dab048cd99c67994e3f1084
Joseph Wardell <jay@jaywardell.me>
2025-02-08 21:06:58 -0500

    added a test to ensure that Accordion outputs a div tag

--- Tests/IgniteTesting/Elements/Accordion.swift
+++ Tests/IgniteTesting/Elements/Accordion.swift
@@ -14,8 +14,23 @@ import Testing
 @Suite("Accordion Tests")
 @MainActor
 struct AccordionTests {
-    @Test("ExampleTest")
-    func example() async throws {
+    
+    @Test("Renders a div tag of class accordion")
+    func outputs_div_with_class_accordion() async throws {
+        let sut = Accordion {}
 
+        let attributes = try #require(sut.render().htmlTagWithCloseTag("div")?.attributes)
+        let classAttribute = try #require(attributes.htmlAttribute(named: "class"))
+        
+        #expect(clss == "accordion")
+    }
+    
+    @Test("ExampleTest")
+    func print_example() async throws {
+        let sut = Accordion {}
+            .openMode(.all)
+        let output = sut.render()
+        
+        print(output)
     }
 }
