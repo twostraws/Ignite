d23e53ec6085ebc0ba1e6dd5de85902b3e85c37e
Joseph Wardell <jay@jaywardell.me>
2025-02-16 19:26:38 -0500

    removed notes used while making tests

--- Tests/IgniteTesting/Extensions/URL-DecodedPath.swift
+++ Tests/IgniteTesting/Extensions/URL-DecodedPath.swift
@@ -10,23 +10,6 @@ import Testing
 
 @testable import Ignite
 
-#warning("Remove this")
-/*
- path(percentEncoded:)
- Returns the path component of the URL, optionally removing any percent-encoding.
- func path(percentEncoded: Bool = true) -> String
- Parameters
- percentEncoded
- A Boolean value that indicates whether the URL percent-encodes any unreserved characters. Defaults to true.
- Return Value
- The path component of the URL, optionally percent-encoding any unreserved characters.
- Discussion
- The system doesn’t allow certain characters in the URL path component, so URL percent-encodes those characters to create a valid URL. Calling this function with percentEncoded = false removes any percent-encoding and returns the unencoded path.
- If the URL’s path component is empty, this method returns an empty string.
- Open in Developer Documentation
- 
- */
-
 /// Tests for the `URL-DecodedPath` extension.
 @Suite("URL-DecodedPath Tests")
 struct Test {
