390ad9568e7aa392ecc55ff3a5dc4590136a4de1
JP Toro <jp@jptoro.dev>
2025-02-19 19:09:27 -0500

    Fix `opacity()` return. Fix `id` merging.

--- Sources/Ignite/Framework/AttributeStore.swift
+++ Sources/Ignite/Framework/AttributeStore.swift
@@ -42,8 +42,11 @@ public class AttributeStore {
         mergedAttributes.events.formUnion(attributes.events)
         mergedAttributes.containerAttributes.formUnion(attributes.containerAttributes)
         mergedAttributes.customAttributes.formUnion(attributes.customAttributes)
-        mergedAttributes.id = attributes.id
         mergedAttributes.columnWidth = attributes.columnWidth
+
+        if attributes.id.isEmpty == false {
+            mergedAttributes.id = attributes.id
+        }
 
         if attributes.tag != nil {
             mergedAttributes.tag = attributes.tag
