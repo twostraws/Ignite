00b2387455873b48857516042446359ae14b01b6
JP Toro <jp@jptoro.dev>
2025-02-21 05:05:00 -0500

    `PassthroughHTML` to `PassthroughElement`.

--- Sources/Ignite/Elements/Group.swift
+++ Sources/Ignite/Elements/Group.swift
@@ -16,7 +16,7 @@
 ///         attributes to multiple elements without affecting the document
 ///         structure. If you need a containing `div` element, use
 ///         ``Section`` instead.
-public struct Group: PassthroughHTML {
+public struct Group: PassthroughElement {
     /// The content and behavior of this HTML.
     public var body: some HTML { self }
 
