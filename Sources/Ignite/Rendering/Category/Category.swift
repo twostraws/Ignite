7a2688ae238417a4225b981c11bdea8c899afb05
JP Toro <jp@jptoro.dev>
2025-02-22 11:35:56 -0500

    Add `CustomStringConvertible` conformance.

--- Sources/Ignite/Rendering/Category/Category.swift
+++ Sources/Ignite/Rendering/Category/Category.swift
@@ -6,9 +6,15 @@
 //
 
 /// The category by which articles on your site can be grouped.
-public protocol Category {
+public protocol Category: CustomStringConvertible {
     /// The name of the category.
     var name: String { get }
     /// An array of content that belongs to this category.
     var content: [Content] { get }
+}
+
+public extension Category {
+    var description: String {
+        name
+    }
 }
