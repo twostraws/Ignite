976168b9eca9d37b8e3331bdc1ef00a3def63da5
JP Toro <jp@jptoro.dev>
2025-03-26 14:06:20 -0400

    Add `alignment` parameter to `Grid`.

--- Sources/Ignite/Types/VerticalAlignment.swift
+++ Sources/Ignite/Types/VerticalAlignment.swift
@@ -14,12 +14,21 @@ public enum VerticalAlignment: Equatable, Sendable {
     /// Align content to the bottom
     case bottom
 
-    /// The Bootstrap class that implements this alignment
-    var flexAlignmentClass: String {
+    /// The Bootstrap class for the container with this alignment
+    var containerAlignmentClass: String {
         switch self {
         case .top: "align-items-start"
         case .center: "align-items-center"
         case .bottom: "align-items-end"
+        }
+    }
+
+    /// The Bootstrap class applied to items inside a container
+    var itemAlignmentClass: String {
+        switch self {
+        case .top: "align-self-start"
+        case .center: "align-self-center"
+        case .bottom: "align-self-end"
         }
     }
 }
