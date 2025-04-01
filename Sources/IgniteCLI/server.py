ccc639ec6937cd642963dcf386b3e65582b357f1
Dorian Grolaux <6209874+MrSkwiggs@users.noreply.github.com>
2025-04-01 13:16:32 +0200

    Fix server crash when content directory is re-generated

--- Sources/IgniteCLI/server.py
+++ Sources/IgniteCLI/server.py
@@ -1,22 +1,24 @@
 import http.server
 import socketserver
 import os
+import sys
 
 class CustomHandler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, directory=None, **kwargs):
+        self.base_directory = directory or os.getcwd()
+        super().__init__(*args, directory=self.base_directory, **kwargs)
+
     def send_error(self, code, message=None, explain=None):
         if code == 404:
-            self.error_message_format = ""
-            error_page = os.path.join(os.getcwd(), '404.html')
+            error_page = os.path.join(self.base_directory, '404.html')
             if os.path.exists(error_page):
                 self.send_response(404)
                 self.send_header('Content-Type', 'text/html')
                 self.end_headers()
                 with open(error_page, 'rb') as f:
                     self.wfile.write(f.read())
-            else:
-                super().send_error(404, "File not found", "The requested URL was not found on this server")
-        else:
-            super().send_error(code, message, explain)
+                return
+        super().send_error(code, message, explain)
 
 if __name__ == '__main__':
     import argparse
@@ -24,8 +26,14 @@ if __name__ == '__main__':
     parser.add_argument('--directory', '-d', default=os.getcwd())
     parser.add_argument('port', nargs='?', type=int, default=8000)
     args = parser.parse_args()
-    
-    os.chdir(args.directory)
-    with socketserver.TCPServer(("", args.port), CustomHandler) as httpd:
+
+    if not os.path.exists(args.directory):
+        print(f"Error: Directory '{args.directory}' does not exist")
+        sys.exit(1)
+
+    with socketserver.TCPServer(("", args.port), lambda *a, **kw: CustomHandler(*a, directory=args.directory, **kw)) as httpd:
         print(f"Serving {args.directory} at port {args.port}")
-        httpd.serve_forever()
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            print("\nServer stopped")
