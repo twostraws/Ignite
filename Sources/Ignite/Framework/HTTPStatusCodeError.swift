74c0ff10242c5a1c578a9ac167279b85390fd9dc
Dorian Grolaux <6209874+MrSkwiggs@users.noreply.github.com>
2025-04-02 23:37:24 +0200

    Improve HTTPStatusCode declarations

--- Sources/Ignite/Framework/HTTPStatusCodeError.swift
+++ Sources/Ignite/Framework/HTTPStatusCodeError.swift
@@ -10,6 +10,12 @@ public struct HTTPStatusCodeError: StatusCodeError {
     public let filename: String
     public let title: String
     public let description: String
+
+    public init(_ code: Int, title: String, description: String) {
+        self.filename = String(code)
+        self.title = title
+        self.description = description
+    }
 }
 
 // MARK: - Default Error Statuses
@@ -17,8 +23,8 @@ public struct HTTPStatusCodeError: StatusCodeError {
 public extension HTTPStatusCodeError {
 
     /// The HTTP status code when a page could not be found.
-    static let notFound: HTTPStatusCodeError = .init(filename: "404", title: "Page Not Found", description: "The page you are looking for could not be found.")
+    static let notFound: HTTPStatusCodeError = .init(404, title: "Page Not Found", description: "The page you are looking for could not be found.")
 
     /// The HTTP status code when an internal server error occurred.
-    static let internalServerError: HTTPStatusCodeError = .init(filename: "500", title: "Internal Server Error", description: "The server encountered an internal error. Please try again later.")
+    static let internalServerError: HTTPStatusCodeError = .init(500, title: "Internal Server Error", description: "The server encountered an internal error. Please try again later.")
 }
