96db5bf67871642f51cc0ebdca9ef22b4086b0df
Paul Hudson <paul.hudson@gmail.com>
2025-05-16 10:48:34 +0100

    Dependency version bump.

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
         .executable(name: "IgniteCLI", targets: ["IgniteCLI"])
     ],
     dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-markdown.git", from: "0.5.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
+        .package(url: "https://github.com/swiftlang/swift-markdown.git", from: "0.6.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.5.0"),
         .package(url: "https://github.com/apple/swift-collections.git", from: "1.1.4"),
         .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.7.5")
     ],
