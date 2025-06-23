e6e388ba0210368ca03d550317035c38bf243d54
J.P. Toro <jp@jptoro.dev>
2025-06-23 15:32:56 -0400

    Remove `badge()` modifier from `BodyElement`.

--- Sources/Ignite/Modifiers/BadgeModifier.swift
+++ Sources/Ignite/Modifiers/BadgeModifier.swift
@@ -5,28 +5,6 @@
 // See LICENSE for license information.
 //
 
-public extension BodyElement {
-    /// Adds a badge to the right side of this element.
-    ///
-    /// Use badges to add small, supplementary information to any inline element.
-    /// The badge will be automatically positioned and styled.
-    ///
-    /// ```swift
-    /// Text("Notifications")
-    ///     .badge(Badge("3"))
-    /// ```
-    ///
-    /// - Parameter badge: The badge to display.
-    /// - Returns: A list item containing the original content and badge.
-    func badge(_ badge: Badge) -> some HTML {
-        ListItem {
-            self
-            badge
-        }
-        .class("d-flex", "justify-content-between", "align-items-center")
-    }
-}
-
 public extension ListItem {
     /// Configures this list item to properly display a badge.
     ///
