9754bc5a248485336d68a220dc5a5959c58c860c
J.P. Toro <jp@jptoro.dev>
2025-06-23 15:26:52 -0400

    Add `InlineHTML`.

---
+++ Sources/Ignite/Framework/InlineHTML.swift
@@ -0,0 +1,37 @@
+//
+// InlineHTML.swift
+// Ignite
+// https://www.github.com/twostraws/Ignite
+// See LICENSE for license information.
+//
+
+/// A type that represents the HTML representation of `InlineElement`.
+public struct InlineHTML<Content: InlineElement>: HTML {
+    /// The body of this HTML element.
+    public var body: some HTML { self }
+
+    /// The standard set of control attributes for HTML elements.
+    public var attributes = CoreAttributes()
+
+    /// The underlying HTML content, unattributed.
+    private var content: Content
+
+    /// The underlying HTML content, attributed.
+    private var attributedContent: Content {
+        var content = content
+        content.attributes.merge(attributes)
+        return content
+    }
+
+    /// Creates a new `InlineHTML` instance that wraps the given HTML content.
+    /// - Parameter content: The HTML content to wrap
+    init(_ content: Content) {
+        self.content = content
+    }
+
+    /// Renders the wrapped HTML content using the given publishing context
+    /// - Returns: The rendered HTML string
+    public func render() -> Markup {
+        attributedContent.render()
+    }
+}
