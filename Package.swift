a88f77d741345acaa343ad7b662a9cbf04cb7a4d
J.P. Toro <jp@jptoro.dev>
2025-06-23 11:24:53 -0400

    Update framework to macOS 14.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Ignite",
-    platforms: [.macOS(.v13)],
+    platforms: [.macOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(name: "Ignite", targets: ["Ignite"]),
