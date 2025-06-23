ac4872a231e0ab520a3fe0ff49e845eceeeeb4a5
J.P. Toro <jp@jptoro.dev>
2025-06-23 12:55:47 -0400

    Drop `MarkupElement` conformance from `HeadElement`.

--- Sources/Ignite/Framework/ElementTypes/HeadElement.swift
+++ Sources/Ignite/Framework/ElementTypes/HeadElement.swift
@@ -7,4 +7,25 @@
 
 /// A metadata element that can exist in the `Head` struct.
 /// - Warning: Do not conform to this type directly.
-public protocol HeadElement: MarkupElement {}
+@MainActor
+public protocol HeadElement: Sendable {
+    /// The standard set of control attributes for HTML elements.
+    var attributes: CoreAttributes { get set }
+
+    /// Renders the head element as markup.
+    /// - Returns: The rendered markup representation.
+    func render() -> Markup
+}
+
+extension HeadElement {
+    /// The publishing context of this site.
+    var publishingContext: PublishingContext {
+        PublishingContext.shared
+    }
+
+    /// Converts this element and its children into an HTML string with attributes.
+    /// - Returns: A string containing the HTML markup
+    func markupString() -> String {
+        render().string
+    }
+}
