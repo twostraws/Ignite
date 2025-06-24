8e556e3027dfa211449b6934ef790bcd4a84979a
J.P. Toro <jp@jptoro.dev>
2025-06-24 12:58:49 -0400

    Make `ZStack` generic.

--- Sources/Ignite/Elements/ZStack.swift
+++ Sources/Ignite/Elements/ZStack.swift
@@ -12,21 +12,18 @@
 /// `ZStack` strips its subviews of implicit styles, such as the bottom margin
 /// automatically applied to paragraphs. All styles explicitly
 /// applied through modifiers like `.margin()` will be respected.
-public struct ZStack: HTML {
+public struct ZStack<Content: HTML>: HTML {
     /// The content and behavior of this HTML.
     public var body: some HTML { self }
 
     /// The standard set of control attributes for HTML elements.
     public var attributes = CoreAttributes()
 
-    /// Whether this HTML belongs to the framework.
-    public var isPrimitive: Bool { true }
-
     /// The alignment point for positioning elements within the stack.
     private var alignment: Alignment
 
     /// The child elements to be stacked.
-    private var items: HTMLCollection
+    private var content: Content
 
     /// Creates a new `ZStack` with the specified alignment and content.
     /// - Parameters:
@@ -34,32 +31,36 @@ public struct ZStack: HTML {
     ///   - items: A closure that returns the elements to be stacked.
     public init(
         alignment: Alignment = .center,
-        @HTMLBuilder _ items: () -> some HTML
+        @HTMLBuilder content: () -> Content
     ) {
-        self.items = HTMLCollection(items)
+        self.content = content()
         self.alignment = alignment
     }
 
     public func render() -> Markup {
-        var items = items.elements
-
-        items = items.enumerated().map { index, item in
-            var elementAttributes = CoreAttributes()
-            elementAttributes.append(classes: "mb-0")
-            elementAttributes.append(styles: [
-                .init(.position, value: "relative"),
-                .init(.gridArea, value: "1/1"),
-                .init(.zIndex, value: "\(index)")
-            ])
-
-            elementAttributes.append(styles: alignment.itemAlignmentRules)
-            return item.attributes(elementAttributes)
-        }
-
         var attributes = attributes
         attributes.append(styles: .init(.display, value: "grid"))
 
-        let contentHTML = items.map { $0.markupString() }.joined()
+        let contentHTML = content
+            .subviews()
+            .enumerated()
+            .map { index, subview in
+                addAttributesToChild(subview, at: index).markupString()
+            }.joined()
+
         return Markup("<div\(attributes)>\(contentHTML)</div>")
+    }
+
+    private func addAttributesToChild(_ child: some HTML, at index: Int) -> some HTML {
+        var elementAttributes = CoreAttributes()
+        elementAttributes.append(classes: "mb-0")
+        elementAttributes.append(styles: [
+            .init(.position, value: "relative"),
+            .init(.gridArea, value: "1/1"),
+            .init(.zIndex, value: "\(index)")
+        ])
+
+        elementAttributes.append(styles: alignment.itemAlignmentRules)
+        return child.attributes(elementAttributes)
     }
 }
