014b3aa0403a7cb798b3c54b5685d9df4b7aeb98
J.P. Toro <jp@jptoro.dev>
2025-06-24 13:03:08 -0400

    Add `HTMLModifier` protocol and `ModifiedContentProxy` type.

---
+++ Sources/Ignite/Modifiers/HTMLModifier.swift
@@ -0,0 +1,28 @@
+//
+// HTMLModifier.swift
+// Ignite
+// https://www.github.com/twostraws/Ignite
+// See LICENSE for license information.
+//
+
+//
+// HTMLModifier.swift
+// Ignite
+// https://www.github.com/twostraws/Ignite
+// See LICENSE for license information.
+//
+
+/// A type that modifies HTML content.
+@MainActor
+public protocol HTMLModifier {
+    /// The type of HTML content this modifier produces.
+    associatedtype Body: HTML
+
+    /// A proxy that provides access to the content being modified.
+    typealias Content = ModifiedContentProxy<Self>
+
+    /// Returns the modified HTML content.
+    /// - Parameter content: The content to modify.
+    /// - Returns: The modified HTML content.
+    @HTMLBuilder func body(content: Content) -> Body
+}
---
+++ Sources/Ignite/Modifiers/ModifiedContentProxy.swift
@@ -0,0 +1,47 @@
+//
+// ModifiedContentProxy.swift
+// Ignite
+// https://www.github.com/twostraws/Ignite
+// See LICENSE for license information.
+//
+
+/// A proxy that applies HTML modifiers to content while optimizing the output structure.
+public struct ModifiedContentProxy<Modifier: HTMLModifier>: HTML {
+    /// The content and behavior of this HTML.
+    public var body: some HTML { self }
+
+    /// Core HTML attributes applied to the content.
+    public var attributes = CoreAttributes()
+
+    /// The modifier to apply to the content.
+    private var modifier: Modifier
+
+    /// The HTML content being modified.
+    private var content: any HTML
+
+    /// Creates a proxy that applies a modifier to HTML content.
+    /// - Parameters:
+    ///   - content: The HTML content to modify.
+    ///   - modifier: The modifier to apply.
+    init<T: HTML>(content: T, modifier: Modifier) {
+        self.modifier = modifier
+        self.content = content
+    }
+
+    /// Renders the modified content, optimizing div wrapping to avoid layout issues.
+    public func render() -> Markup {
+        if content.isPrimitive {
+            var content = content
+            content.attributes.merge(attributes)
+            return content.render()
+        } else if content.body.isPrimitive, content.markupString().hasPrefix("<div") {
+            // Unnecessarily adding an extra <div> can break positioning
+            // contexts and advanced flex layouts.
+            var content = content.body
+            content.attributes.merge(attributes)
+            return content.render()
+        } else {
+            return Markup("<div\(attributes)>\(content.markupString())</div>")
+        }
+    }
+}
