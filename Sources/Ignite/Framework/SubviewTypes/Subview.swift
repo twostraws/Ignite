87d5e4211b8f3e48fddd3bb0aa9ee8686139edb4
J.P. Toro <jp@jptoro.dev>
2025-06-23 21:19:07 -0400

    Add `Subview` type.

---
+++ Sources/Ignite/Framework/SubviewTypes/Subview.swift
@@ -0,0 +1,43 @@
+//
+// Subview.swift
+// Ignite
+// https://www.github.com/twostraws/Ignite
+// See LICENSE for license information.
+//
+
+/// An opaque value representing the child of another view.
+struct Subview: HTML {
+    /// The content and behavior of this HTML.
+    var body: some HTML { self }
+
+    /// The standard set of control attributes for HTML elements.
+    var attributes = CoreAttributes()
+
+    /// The underlying HTML content, unattributed.
+    private var content: any HTML
+
+    /// The underlying HTML content, with attributes.
+    var wrapped: any HTML {
+        var wrapped = content
+        wrapped.attributes.merge(attributes)
+        return wrapped
+    }
+
+    /// Creates a new `Subview` instance that wraps the given HTML content.
+    /// - Parameter content: The HTML content to wrap
+    init(_ wrapped: any HTML) {
+        self.content = wrapped
+    }
+
+    nonisolated func render() -> Markup {
+        MainActor.assumeIsolated {
+           wrapped.render()
+        }
+    }
+}
+
+extension Subview: Equatable {
+    nonisolated static func == (lhs: Subview, rhs: Subview) -> Bool {
+        lhs.render() == rhs.render()
+    }
+}
