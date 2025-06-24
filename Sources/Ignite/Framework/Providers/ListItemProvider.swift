3d0bc91ac5b4cf1f3721b80171ccc1239e011b26
J.P. Toro <jp@jptoro.dev>
2025-06-24 11:58:25 -0400

    Add `ListItemProvider` protocol.

---
+++ Sources/Ignite/Framework/Providers/ListItemProvider.swift
@@ -0,0 +1,11 @@
+//
+// ListItemProvider.swift
+// Ignite
+// https://www.github.com/twostraws/Ignite
+// See LICENSE for license information.
+//
+
+/// A type that does not need to be wrapped in <li>
+/// if processed in a List.
+@MainActor
+protocol ListItemProvider {}
