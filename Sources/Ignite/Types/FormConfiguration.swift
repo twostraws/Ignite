b70b0ac11f920cb61f2335437569fbcdf71425e2
J.P. Toro <jp@jptoro.dev>
2025-06-23 21:11:59 -0400

    Add `FormConfiguration` type.

---
+++ Sources/Ignite/Types/FormConfiguration.swift
@@ -0,0 +1,21 @@
+//
+// FormConfiguration.swift
+// Ignite
+// https://www.github.com/twostraws/Ignite
+// See LICENSE for license information.
+//
+
+/// Configuration options for customizing a form's layout and appearance.
+struct FormConfiguration: Sendable {
+    /// The number of columns in the form's grid layout.
+    var columnCount: Int = 12
+
+    /// The amount of space between form elements.
+    var spacing: SpacingAmount = .medium
+
+    /// The display style for form control labels.
+    var labelStyle: ControlLabelStyle = .floating
+
+    /// The size of controls within the form.
+    var controlSize: ControlSize = .medium
+}
