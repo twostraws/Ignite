39319dc8d6abc53745441747e2cf758958c54b11
J.P. Toro <jp@jptoro.dev>
2025-06-24 12:53:22 -0400

    Make `Time` generic.

--- Sources/Ignite/Elements/Time.swift
+++ Sources/Ignite/Elements/Time.swift
@@ -10,27 +10,24 @@ import Foundation
 /// Represents a specific period in time. It may include the datetime attribute
 /// to translate dates into machine-readable format, allowing for better search
 /// engine results or custom features such as reminders.
-public struct Time: InlineElement {
+public struct Time<Content: InlineElement>: InlineElement {
     /// The content and behavior of this HTML.
-    public var body: some InlineElement { self }
+    public var body: Never { fatalError() }
 
     /// The standard set of control attributes for HTML elements.
     public var attributes = CoreAttributes()
 
-    /// Whether this HTML belongs to the framework.
-    public var isPrimitive: Bool { true }
-
     /// The contents of this time tag.
-    public var contents: any InlineElement
+    private var content: Content
 
     /// This attribute indicates the time and/or date of the element.
-    public var dateTime: Date?
+    private var dateTime: Date?
 
     /// Creates a time element with no content.
     /// - Parameter dateTime: The time and/or date of the element
     /// inside the time element.
-    public init(dateTime: Date? = nil) {
-        self.contents = EmptyInlineElement()
+    public init(dateTime: Date? = nil) where Content == EmptyInlineElement {
+        self.content = EmptyInlineElement()
         self.dateTime = dateTime
     }
 
@@ -38,8 +35,8 @@ public struct Time: InlineElement {
     /// - Parameter singleElement: The element you want to place
     /// - Parameter dateTime: The time and/or date of the element
     /// inside the time element.
-    public init(_ singleElement: any InlineElement, dateTime: Date? = nil) {
-        self.contents = singleElement
+    public init(_ singleElement: Content, dateTime: Date? = nil) {
+        self.content = singleElement
         self.dateTime = dateTime
     }
 
@@ -47,9 +44,9 @@ public struct Time: InlineElement {
     /// elements to place inside the time element.
     /// - Parameter dateTime: The time and/or date of the element
     /// - Parameter contents: The elements to place inside the time element.
-    public init(dateTime: Date? = nil, @HTMLBuilder contents: () -> some InlineElement) {
+    public init(dateTime: Date? = nil, @HTMLBuilder content: () -> Content) {
         self.dateTime = dateTime
-        self.contents = contents()
+        self.content = content()
     }
 
     /// Renders this element using publishing context passed in.
@@ -59,7 +56,7 @@ public struct Time: InlineElement {
         if let dateTime {
             attributes.append(customAttributes: .init(name: "datetime", value: dateTime.asISO8601))
         }
-        let contentHTML = contents.markupString()
+        let contentHTML = content.markupString()
         return Markup("<time\(attributes)>\(contentHTML)</time>")
     }
 }
