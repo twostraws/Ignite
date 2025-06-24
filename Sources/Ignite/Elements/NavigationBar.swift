727fc7cb6b9ba123ca915575af2ac9891f32a962
J.P. Toro <jp@jptoro.dev>
2025-06-23 22:06:25 -0400

    Remove `NavigationItemGroup` type.

--- Sources/Ignite/Elements/NavigationBar.swift
+++ Sources/Ignite/Elements/NavigationBar.swift
@@ -199,8 +199,6 @@ public struct NavigationBar: HTML {
     /// Renders this element using publishing context passed in.
     /// - Returns: The HTML for this element.
     public func render() -> Markup {
-        // Use the child items directly so that types like Spacer() aren't concealed
-        let items = items.flatMap { ($0 as? NavigationItemGroup)?.items ?? [$0] }
         let pinnedItems = items.filter { $0.navigationBarVisibility == .always }
         let collapsibleItems = items.filter { $0.navigationBarVisibility == .automatic }
 
