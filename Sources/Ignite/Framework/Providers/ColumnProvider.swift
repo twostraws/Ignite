b89be2277e993bcc0f7e33705e1f4dd98e6258ba
J.P. Toro <jp@jptoro.dev>
2025-06-24 11:27:18 -0400

    Add `ColumnProvider` protocol.

---
+++ Sources/Ignite/Framework/Providers/ColumnProvider.swift
@@ -0,0 +1,9 @@
+//
+// ColumnProvider.swift
+// Ignite
+// https://www.github.com/twostraws/Ignite
+// See LICENSE for license information.
+//
+
+/// A type that has a `Column` as its root view.
+protocol ColumnProvider {}
