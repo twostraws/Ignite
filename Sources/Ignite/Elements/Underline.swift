60d2a1236aacb19a4d26a6681c1da3078fdeda72
J.P. Toro <jp@jptoro.dev>
2025-06-24 12:53:53 -0400

    Make `Underline` generic.

--- Sources/Ignite/Elements/Underline.swift
+++ Sources/Ignite/Elements/Underline.swift
@@ -6,29 +6,26 @@
 //
 
 /// Renders text with an underline.
-public struct Underline: InlineElement {
+public struct Underline<Content: InlineElement>: InlineElement {
     /// The content and behavior of this HTML.
-    public var body: some InlineElement { self }
+    public var body: Never { fatalError() }
 
     /// The standard set of control attributes for HTML elements.
     public var attributes = CoreAttributes()
 
-    /// Whether this HTML belongs to the framework.
-    public var isPrimitive: Bool { true }
-
     /// The content that should be underlined.
-    var content: any InlineElement
+    var content: Content
 
     /// Creates a new `Underline` instance using an inline element builder
     /// that returns an array of content to place inside.
-    public init(@InlineElementBuilder content: @escaping () -> some InlineElement) {
+    public init(@InlineElementBuilder content: () -> Content) {
         self.content = content()
     }
 
     /// Creates a new `Underline` instance using one `InlineElement`
     /// that should be rendered with a strikethrough effect.
     /// - Parameter singleElement: The element to strike.
-    public init(_ singleElement: some InlineElement) {
+    public init(_ singleElement: Content) {
         self.content = singleElement
     }
 
