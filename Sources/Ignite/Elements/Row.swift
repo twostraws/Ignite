512b2f71811f80499a1ea69798fe1b699316c2b0
J.P. Toro <jp@jptoro.dev>
2025-06-24 11:38:45 -0400

    Update `Row` to use generics.

--- Sources/Ignite/Elements/Row.swift
+++ Sources/Ignite/Elements/Row.swift
@@ -6,38 +6,36 @@
 //
 
 /// One row inside a `Table`.
-public struct Row: HTML {
+public struct Row<Content: HTML>: HTML {
     /// The content and behavior of this HTML.
     public var body: some HTML { self }
 
     /// The standard set of control attributes for HTML elements.
     public var attributes = CoreAttributes()
 
-    /// Whether this HTML belongs to the framework.
-    public var isPrimitive: Bool { true }
-
     /// The columns to display inside this row.
-    private var columns: HTMLCollection
+    private var columns: Content
 
     /// Create a new `Row` using a page element builder that returns the
     /// array of columns to use in this row.
     /// - Parameter columns: The columns to use in this row.
-    public init(@HTMLBuilder columns: () -> some HTML) {
-        self.columns = HTMLCollection(columns)
+    public init(@HTMLBuilder columns: () -> Content) {
+        self.columns = columns()
     }
 
     /// Renders this element using publishing context passed in.
     /// - Returns: The HTML for this element.
     public func render() -> Markup {
-        let output = columns.map { column in
-            if column is Column {
+        let output = columns.subviews().map { column in
+            if column.wrapped is any ColumnProvider {
                 column.render()
             } else {
                 Markup("<td>\(column.markupString())</td>")
             }
         }.joined()
-        .string
 
-        return Markup("<tr\(attributes)>\(output)</tr>")
+        return Markup("<tr\(attributes)>\(output.string)</tr>")
     }
 }
+
+extension Row: TableElement {}
