5eab86565060b21b1db9c24b5d19b5626af898e0
J.P. Toro <jp@jptoro.dev>
2025-06-24 15:26:28 -0400

    Move `Cursor` to own file.

--- Sources/Ignite/Modifiers/CursorModifier.swift
+++ Sources/Ignite/Modifiers/CursorModifier.swift
@@ -5,23 +5,6 @@
 // See LICENSE for license information.
 //
 
-public enum Cursor: String, CaseIterable, Sendable {
-    /// The cursor to display based on the current context. E.g., equivalent to text when hovering text.
-    case auto
-
-    /// Default cursor. Typically an arrow.
-    case `default`
-
-    /// No cursor is rendered.
-    case pointer
-
-    /// Something can be zoomed (magnified) in.
-    case zoomIn = "zoom-in"
-
-    /// Something can be zoomed (magnified) out.
-    case zoomOut = "zoom-out"
-}
-
 public extension HTML {
     /// Changes the cursor appearance of the element when hovering over the element.
     /// - Parameter cursor: The desired cursor style.
