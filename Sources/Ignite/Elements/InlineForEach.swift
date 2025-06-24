5f3ee929a1bf57b355c1078f264633c44387929e
J.P. Toro <jp@jptoro.dev>
2025-06-24 12:23:21 -0400

    Make `InlineForEach` generic.

--- Sources/Ignite/Elements/InlineForEach.swift
+++ Sources/Ignite/Elements/InlineForEach.swift
@@ -7,35 +7,33 @@
 
 /// A structure that creates inline content by mapping over a sequence of data.
 @MainActor
-public struct InlineForEach<Data: Sequence>: InlineElement, PassthroughElement {
+public struct InlineForEach<Data: Sequence, Content: InlineElement>: InlineElement {
+    /// The content and behavior of this HTML.
+    public var body: Never { fatalError() }
+
     /// The standard set of control attributes for HTML elements.
     public var attributes = CoreAttributes()
-
-    /// Whether this HTML belongs to the framework.
-    public var isPrimitive: Bool { true }
 
     /// The sequence of data to iterate over.
     private let data: Data
 
     /// The child elements contained within this HTML element.
-    var items: InlineElementCollection
-
-    public var body: some InlineElement { self }
+    private var items: [Content]
 
     /// Creates a new InlineForEach instance that generates inline content from a sequence.
     /// - Parameters:
     ///   - data: The sequence to iterate over.
     ///   - content: A closure that converts each element into inline content.
-    public init(_ data: Data, @InlineElementBuilder content: @escaping (Data.Element) -> some InlineElement) {
+    public init(_ data: Data, @InlineElementBuilder content: @escaping (Data.Element) -> Content) {
         self.data = data
-        self.items = InlineElementCollection(data.map(content))
+        self.items = data.map(content)
     }
 
     /// Renders the ForEach content.
     /// - Returns: The rendered HTML string.
     public func render() -> Markup {
         items.map {
-            var item: any InlineElement = $0
+            var item = $0
             item.attributes.merge(attributes)
             return item.render()
         }.joined()
