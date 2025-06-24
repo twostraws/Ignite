396fe094daebd574c7b6f3bcc65178347e6cd553
J.P. Toro <jp@jptoro.dev>
2025-06-24 12:03:13 -0400

    Make `ListItem` generic. Add back `badge()` modifier.

--- Sources/Ignite/Elements/ListItem.swift
+++ Sources/Ignite/Elements/ListItem.swift
@@ -8,18 +8,27 @@
 /// Creates one item in a list. This isn't always needed, because you can place other
 /// elements directly into lists if you wish. Use `ListItem` when you specifically
 /// need a styled HTML <li> element.
-public struct ListItem: HTML, ListableElement {
+public struct ListItem<Content: HTML, BadgeContent: InlineElement>: HTML {
     /// The content and behavior of this HTML.
     public var body: some HTML { self }
 
     /// The standard set of control attributes for HTML elements.
     public var attributes = CoreAttributes()
 
-    /// Whether this HTML belongs to the framework.
-    public var isPrimitive: Bool { true }
-
     /// The content of this list item.
-    private var content: any HTML
+    private var content: Content
+
+    /// The badge for this list item.
+    private var badge: Badge<BadgeContent>?
+
+    /// Configures this list item to properly display a badge.
+    /// - Parameter badge: The badge to display.
+    /// - Returns: A modified list item with proper badge styling.
+    public func badge<T: InlineElement>(_ badge: Badge<T>) -> some HTML {
+        var item = ListItem<Content, T>(self.content, badge: badge)
+        item.attributes.append(classes: "d-flex", "justify-content-between", "align-items-center")
+        return item
+    }
 
     /// Sets the role for this list item, which controls its appearance.
     /// - Parameter role: The new role to apply.
@@ -34,22 +43,38 @@ public struct ListItem: HTML, ListableElement {
     /// Creates a new `ListItem` object using an inline element builder that
     /// returns an array of `HTML` objects to display in the list.
     /// - Parameter content: The content you want to display in your list.
-    public init(@HTMLBuilder content: () -> some HTML) {
+    public init(@HTMLBuilder content: () -> Content) where BadgeContent == EmptyInlineElement {
         self.content = content()
+    }
+
+    /// Creates a new `ListItem` object from a peice of HTML content.
+    /// - Parameter content: The content you want to display in your list.
+    init(_ content: Content) where BadgeContent == EmptyInlineElement {
+        self.content = content
+    }
+
+    /// Creates a new `ListItem` object from a peice of HTML content.
+    /// - Parameters:
+    ///    - content: The content you want to display in your list.
+    ///    - badge: The badge to display at the end of the item.
+    init(_ content: Content, badge: Badge<BadgeContent>) {
+        self.content = content
+        self.badge = badge
+    }
+
+    /// Creates a new `ListItem` object from a peice of HTML content.
+    /// - Parameter content: The content you want to display in your list.
+    init<T: InlineElement>(_ content: T) where Content == InlineHTML<T>, BadgeContent == EmptyInlineElement {
+        self.content = InlineHTML(content)
     }
 
     /// Renders this element using publishing context passed in.
     /// - Returns: The HTML for this element.
     public func render() -> Markup {
         let contentHTML = content.markupString()
-        return Markup("<li\(attributes)>\(contentHTML)</li>")
-    }
-
-    /// Renders this element inside a list, using the publishing context passed in.
-    /// - Returns: The HTML for this element.
-    public func listMarkup() -> Markup {
-        // We do nothing special here, so just send back
-        // the default rendering.
-        render()
+        let badgeHTML = badge?.markupString() ?? ""
+        return Markup("<li\(attributes)>\(contentHTML)\(badgeHTML)</li>")
     }
 }
+
+extension ListItem: ListItemProvider {}
