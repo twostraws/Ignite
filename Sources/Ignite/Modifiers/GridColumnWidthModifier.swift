b532384539b851d6f63539d9e8439cdacaa8d04b
J.P. Toro <jp@jptoro.dev>
2025-06-24 16:26:00 -0400

    Update `gridCellColumns()` implementation.

--- Sources/Ignite/Modifiers/GridColumnWidthModifier.swift
+++ Sources/Ignite/Modifiers/GridColumnWidthModifier.swift
@@ -5,22 +5,33 @@
 // See LICENSE for license information.
 //
 
-@MainActor private func gridColumnWidthModifier(
-    _ width: ColumnWidth,
-    content: any HTML
-) -> AnyHTML {
-    if content.isPrimitive {
-        AnyHTML(content.class(width.className))
-    } else {
-        AnyHTML(Section(content).class(width.className))
+public extension HTML {
+    /// Adjusts the number of columns assigned to this element.
+    /// - Parameter width: The new number of columns to use.
+    /// - Returns: A new element with the adjusted column width.
+    func gridCellColumns(_ width: Int) -> some HTML {
+        self.style(.gridColumn, "span \(width)")
     }
 }
+
+public extension InlineElement {
+    /// Adjusts the number of columns assigned to this element.
+    /// - Parameter width: The new number of columns to use.
+    /// - Returns: A new element with the adjusted column width.
+    func gridCellColumns(_ width: Int) -> some InlineElement {
+        self.style(.gridColumn, "span \(width)")
+    }
+}
+
 
 public extension HTML {
     /// Adjusts the number of columns assigned to this element.
     /// - Parameter width: The new number of columns to use.
     /// - Returns: A new element with the adjusted column width.
+    @available(*, deprecated, message: """
+    Please use the gridCellColumns() modifier. This method will be removed in a future release.
+    """)
     func width(_ width: Int) -> some HTML {
-        gridColumnWidthModifier(.count(width), content: self)
+        gridCellColumns(width)
     }
 }
