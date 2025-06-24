1fb5a18c68357c2f4d08091b855e9c64ec5a08ec
J.P. Toro <jp@jptoro.dev>
2025-06-24 12:49:21 -0400

    Make `Strong` generic.

--- Sources/Ignite/Elements/Strong.swift
+++ Sources/Ignite/Elements/Strong.swift
@@ -6,29 +6,26 @@
 //
 
 /// Renders text with a strong text effect, which usually means bold.
-public struct Strong: InlineElement {
+public struct Strong<Content: InlineElement>: InlineElement {
     /// The content and behavior of this HTML.
-    public var body: some InlineElement { self }
+    public var body: Never { fatalError() }
 
     /// The standard set of control attributes for HTML elements.
     public var attributes = CoreAttributes()
 
-    /// Whether this HTML belongs to the framework.
-    public var isPrimitive: Bool { true }
-
     /// The content that should be strengthened.
-    var content: any InlineElement
+    private var content: Content
 
     /// Creates a new `Strong` instance using an inline element builder
     /// that returns an array of content to place inside.
-    public init(@InlineElementBuilder content: () -> some InlineElement) {
+    public init(@InlineElementBuilder content: () -> Content) {
         self.content = content()
     }
 
     /// Creates a new `Strong` instance using one `InlineElement`
     /// that should be rendered with a strong effect.
     /// - Parameter singleElement: The element to strengthen.
-    public init(_ singleElement: any InlineElement) {
+    public init(_ singleElement: Content) {
         self.content = singleElement
     }
 
