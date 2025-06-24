fbef924ef3158ac45f73a4fa84e0f34dfddff954
J.P. Toro <jp@jptoro.dev>
2025-06-24 11:35:56 -0400

    Update `ForEach` to use generics.

--- Sources/Ignite/Elements/ForEach.swift
+++ Sources/Ignite/Elements/ForEach.swift
@@ -7,19 +7,19 @@
 
 /// A structure that creates HTML content by mapping over a sequence of data.
 @MainActor
-public struct ForEach<Data: Sequence>: HTML, ListableElement, PassthroughElement {
+public struct ForEach<Data: Sequence, Content> {
     /// The standard set of control attributes for HTML elements.
     public var attributes = CoreAttributes()
-
-    /// Whether this HTML belongs to the framework.
-    public var isPrimitive: Bool { true }
 
     /// The sequence of data to iterate over.
     private let data: Data
 
     /// The child elements contained within this HTML element.
-    var items: HTMLCollection
+    var items: [Content]
+}
 
+extension ForEach: HTML, BodyElement, MarkupElement, VariadicHTML, Sendable where Content: HTML {
+    /// The content and behavior of this HTML.
     public var body: some HTML { self }
 
     /// Creates a new ForEach instance that generates HTML content from a sequence.
@@ -28,26 +28,45 @@ public struct ForEach<Data: Sequence>: HTML, ListableElement, PassthroughElement
     ///   - content: A closure that converts each element into HTML content.
     public init(
         _ data: Data,
-        @HTMLBuilder content: @escaping (Data.Element) -> some HTML
+        @HTMLBuilder content: (Data.Element) -> Content
     ) {
         self.data = data
-        self.items = HTMLCollection(data.map(content))
+        self.items = data.map(content)
+    }
+
+    var subviews: SubviewsCollection {
+        SubviewsCollection(items.map(Subview.init))
+    }
+
+    /// Renders the ForEach content when this isn't part of a list.
+    /// - Returns: The rendered HTML string.
+    public func render() -> Markup {
+        subviews.attributes(attributes).render()
+    }
+}
+
+extension ForEach: TableElement where Content: TableElement {
+    /// Creates a new ForEach instance that generates HTML content from a sequence.
+    /// - Parameters:
+    ///   - data: The sequence to iterate over.
+    ///   - content: A closure that converts each element into HTML content.
+    init(
+        _ data: Data,
+        @TableElementBuilder content: @escaping (Data.Element) -> Content
+    ) {
+        self.data = data
+        self.items = data.map(content)
     }
 
     /// Renders the ForEach content when this isn't part of a list.
     /// - Returns: The rendered HTML string.
     public func render() -> Markup {
         items.map {
-            var item: any BodyElement = $0
+            var item = $0
             item.attributes.merge(attributes)
             return item.render()
         }.joined()
     }
-
-    /// Renders the ForEach content when this isn't part of a list.
-    /// - Returns: The rendered HTML string.
-    func listMarkup() -> Markup {
-        // ListableElement conformance ensures other views never wrap ForEach in <li> tags.
-        render()
-    }
 }
+
+extension ForEach: ColumnProvider where Content: ColumnProvider {}
