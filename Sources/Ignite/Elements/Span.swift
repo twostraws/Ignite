efa7c2e0f7a79d84d22a890b5770f5f0bdb552d3
J.P. Toro <jp@jptoro.dev>
2025-06-24 22:33:30 -0400

    Make `Span` generic.

--- Sources/Ignite/Elements/Span.swift
+++ Sources/Ignite/Elements/Span.swift
@@ -7,43 +7,43 @@
 
 /// An inline subsection of another element, useful when you need to style
 /// just part of some text, for example.
-public struct Span: InlineElement, NavigationElement, ControlGroupElement {
+public struct Span<Content: InlineElement>: InlineElement, NavigationElement, ControlGroupElement {
     /// The content and behavior of this HTML.
     public var body: Never { fatalError() }
 
     /// The standard set of control attributes for HTML elements.
     public var attributes = CoreAttributes()
 
-    /// The contents of this span.
-    private var contents: any InlineElement
+    /// The content of this span.
+    private var content: Content
 
     /// How a `NavigationBar` displays this item at different breakpoints.
     public var navigationBarVisibility: NavigationBarVisibility = .automatic
 
     /// Creates a span with no content. Used in some situations where
     /// exact styling is performed by Bootstrap, e.g. in Carousel.
-    public init() {
-        self.contents = EmptyInlineElement()
+    public init() where Content == EmptyInlineElement {
+        self.content = EmptyInlineElement()
     }
 
     /// Creates a span from one `InlineElement`.
     /// - Parameter singleElement: The element you want to place
     /// inside the span.
-    public init(_ singleElement: some InlineElement) {
-        self.contents = singleElement
+    public init(_ singleElement: Content) {
+        self.content = singleElement
     }
 
     /// Creates a span from an inline element builder that returns an array of
     /// elements to place inside the span.
     /// - Parameter contents: The elements to place inside the span.
-    public init(@InlineElementBuilder contents: () -> some InlineElement) {
-        self.contents = contents()
+    public init(@InlineElementBuilder content: () -> Content) {
+        self.content = content()
     }
 
     /// Renders this element using publishing context passed in.
     /// - Returns: The HTML for this element.
     public func render() -> Markup {
-        let contentHTML = contents.markupString()
+        let contentHTML = content.markupString()
         return Markup("<span\(attributes)>\(contentHTML)</span>")
     }
 }
