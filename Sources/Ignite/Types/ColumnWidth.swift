73caa68999405d83c3f2fd683412c868d8b024b0
J.P. Toro <jp@jptoro.dev>
2025-06-24 20:58:35 -0400

    Reimplement `ColumnWidth` as enum.

--- Sources/Ignite/Types/ColumnWidth.swift
+++ Sources/Ignite/Types/ColumnWidth.swift
@@ -5,21 +5,32 @@
 // See LICENSE for license information.
 //
 
-/// Controls how many columns a given block element takes up in a `Section`.
-struct ColumnWidth: Equatable, Sendable {
-    /// The Bootstrap class name for the column width.
-    var className: String
-
-    /// The column will expand to fill available space, distributing evenly with other `.uniform` columns.
-    /// For example, if there are three `.uniform` elements in a 12-column section,
-    /// each will automatically take up four columns.
-    static let uniform = ColumnWidth(className: "col")
+/// Controls how many columns a given block element takes up in a `Grid`.
+enum ColumnWidth {
+    /// The column will expand to fill available space,
+    /// distributing evenly with other `.uniform` columns.
+    /// For example, if there are three `.uniform`elements
+    /// in a 12-column section, each will automatically
+    /// take up four columns.
+    case uniform
 
     /// The columns should be sized based on its content.
-    static let intrinsic = ColumnWidth(className: "col-auto")
+    case variable
 
     /// This element should take up a precise number of columns.
-    static func count(_ width: Int) -> Self {
-        .init(className: "col-md-\(width)")
+    case count(Int)
+
+    /// The Bootstrap class name for the column width.
+    var className: String {
+        switch self {
+        case .uniform: "col"
+        case .variable: "col-auto"
+        case .count(let width): "col-md-\(width)"
+        }
+    }
+
+    /// Returns the Bootstrap class name for the column width.
+    func callAsFunction() -> String {
+        className
     }
 }
